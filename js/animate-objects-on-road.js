4938f7b6b8827c770e6a8b32f1f539e7fd76c9ca
25maae <eaa25maae@students.eaaa.dk>
2025-11-05 09:49:39 +0100

    js

---
+++ js/animate-objects-on-road.js
@@ -0,0 +1,7 @@
+"use strict"
+
+// Hent DOM elementer
+const getRedCar = document.querySelector('.car1');
+const getPoliceCar = document.querySelector('.car2');
+const getBlueCar = document.querySelector('.car3');
+
