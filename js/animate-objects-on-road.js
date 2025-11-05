1c7247e37e3b4fd1d60da00e2d3f49ab816eb941
25maae <eaa25maae@students.eaaa.dk>
2025-11-05 09:55:16 +0100

    Update animate-objects-on-road.js

--- js/animate-objects-on-road.js
+++ js/animate-objects-on-road.js
@@ -4,3 +4,5 @@
 const getRedCar = document.getElementById("redCar");
 const getPoliceCar = document.getElementById("policeCar");
 const getBlueCar = document.getElementById("blueCar");
+const getSun = document.querySelector(".sun");
+const getScene = document.querySelector(".scene");
