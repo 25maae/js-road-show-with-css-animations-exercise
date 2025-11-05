6c65ff8b9ffcfdc653f5b7d7580513a5fbc8ea70
25maae <eaa25maae@students.eaaa.dk>
2025-11-05 10:17:12 +0100

    Update animate-objects-on-road.js

--- js/animate-objects-on-road.js
+++ js/animate-objects-on-road.js
@@ -9,6 +9,28 @@ const sun = document.querySelector(".sun");
 const scene = document.querySelector(".scene");
 
 // Opretter lydobjekter
-const soundRedCar = new Audio("sound/red-car-horn.wav");
-const soundPoliceCar = new Audio("sound/police-car-sound.wav");
-const soundBlueCar = new Audio("sound/blue-car-sound.wav");
+const soundRedCar = new Audio();
+soundRedCar.src = "../sound/red-car-horn.wav";
+const soundPoliceCar = new Audio();
+soundPoliceCar.src = "../sound/police-car-sound.wav";
+const soundBlueCar = new Audio();
+soundBlueCar.src = "../sound/blue-car-sound.wav";
+
+// Funktion til at afspille lyd ved klik på bil
+if (getRedCar) {
+  getRedCar.addEventListener("click", () => {
+    soundRedCar.play();
+  });
+}
+
+if (getPoliceCar) {
+  getPoliceCar.addEventListener("click", () => {
+    soundPoliceCar.play();
+  });
+}
+
+if (getBlueCar) {
+  getBlueCar.addEventListener("click", () => {
+    soundBlueCar.play();
+  });
+}
