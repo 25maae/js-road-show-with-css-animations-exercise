52b6e3c1444be8c34167e3c079ae2d3599dd0684
25maae <eaa25maae@students.eaaa.dk>
2025-11-06 11:17:35 +0100

    Update animate-objects-on-road.js

--- js/animate-objects-on-road.js
+++ js/animate-objects-on-road.js
@@ -28,47 +28,50 @@ const carInfo = [
   },
 ];
 
-// Hent DOM elementer
-const getRedCar = document.getElementById("redCar");
-const getPoliceCar = document.getElementById("policeCar");
-const getBlueCar = document.getElementById("blueCar");
+// Venter på at DOM er loaded
+document.addEventListener("DOMContentLoaded", () => {
+  // Hent DOM elementer
+  const getRedCar = document.getElementById("redCar");
+  const getPoliceCar = document.getElementById("policeCar");
+  const getBlueCar = document.getElementById("blueCar");
 
-const sun = document.querySelector(".sun");
-const scene = document.querySelector(".scene");
+  const sun = document.querySelector(".sun");
+  const scene = document.querySelector(".scene");
 
-// Opretter lydobjekter
-const soundRedCar = new Audio();
-soundRedCar.src = "../sound/red-car-horn.wav";
-const soundPoliceCar = new Audio();
-soundPoliceCar.src = "../sound/police-car-sound.wav";
-const soundBlueCar = new Audio();
-soundBlueCar.src = "../sound/blue-car-sound.wav";
+  // Opretter lydobjekter
+  const soundRedCar = new Audio();
+  soundRedCar.src = "../sound/red-car-horn.wav";
+  const soundPoliceCar = new Audio();
+  soundPoliceCar.src = "../sound/police-car-sound.wav";
+  const soundBlueCar = new Audio();
+  soundBlueCar.src = "../sound/blue-car-sound.wav";
 
-// Funktion til at afspille lyd ved klik på bil
-if (getRedCar) {
-  getRedCar.style.cursor = "pointer"; // Ændrer cursor til pointer ved hover
-  getRedCar.addEventListener("click", () => {
-    soundRedCar.play();
-  });
-}
+  // Funktion til at afspille lyd ved klik på bil
+  if (getRedCar) {
+    getRedCar.style.cursor = "pointer"; // Ændrer cursor til pointer ved hover
+    getRedCar.addEventListener("click", () => {
+      soundRedCar.play();
+    });
+  }
 
-if (getPoliceCar) {
-  getPoliceCar.style.cursor = "pointer"; // Ændrer cursor til pointer ved hover
-  getPoliceCar.addEventListener("click", () => {
-    soundPoliceCar.play();
-  });
-}
+  if (getPoliceCar) {
+    getPoliceCar.style.cursor = "pointer"; // Ændrer cursor til pointer ved hover
+    getPoliceCar.addEventListener("click", () => {
+      soundPoliceCar.play();
+    });
+  }
 
-if (getBlueCar) {
-  getBlueCar.style.cursor = "pointer"; // Ændrer cursor til pointer ved hover
-  getBlueCar.addEventListener("click", () => {
-    soundBlueCar.play();
-  });
-}
+  if (getBlueCar) {
+    getBlueCar.style.cursor = "pointer"; // Ændrer cursor til pointer ved hover
+    getBlueCar.addEventListener("click", () => {
+      soundBlueCar.play();
+    });
+  }
 
-// Funktion til at ændre fra dag til nat ved klik på solen
-if (sun && scene) {
-  sun.addEventListener("click", () => {
-    scene.classList.toggle("night");
-  });
-}
+  // Funktion til at ændre fra dag til nat ved klik på solen
+  if (sun && scene) {
+    sun.addEventListener("click", () => {
+      scene.classList.toggle("night");
+    });
+  }
+});
